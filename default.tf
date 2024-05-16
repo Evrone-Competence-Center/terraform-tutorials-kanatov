3e5184e175eb19d02128b9a131cfc4e87767c162
Vladislav <vlad.kanatik@ya.ru>
2024-05-16 14:38:03 +0300

    edit .tf files

--- default.tf
+++ default.tf
@@ -7,14 +7,15 @@ terraform {
   required_version = ">= 0.13"
 }
 
-locals {
-  cloud_id = "b1g1f73gcm5vet9spf42"
-  folder_id = "b1gds5v86pf14bpd23vd"
-}
+# locals {
+#   cloud_id = "b1g1f73gcm5vet9spf42"
+#   folder_id = "b1gds5v86pf14bpd23vd"
+# }
 
 provider "yandex" {
   zone = "ru-central1-a"
-  cloud_id = local.cloud_id
-  folder_id = local.folder_id
-  service_account_key_file = "authorized_key.json"
+  cloud_id = var.cloud_id
+  folder_id = var.folder_id
+#   service_account_key_file = "authorized_key.json"
+  token = var.yc_token
 }
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "yc_token" {
+  type = string
+  description = "IAM-token for auth to YC"
+}
+
+variable "cloud_id" {
+  type = string
+  description = "Cloud ID for YC"
+}
+
+variable "folder_id" {
+  type = string
+  description = "Folder ID for YC"
+}
