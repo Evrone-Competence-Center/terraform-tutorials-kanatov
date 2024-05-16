783b5c40accc36bd72cc77d2cf1ee31676e0f445
Vladislav <vlad.kanatik@ya.ru>
2024-05-16 17:43:43 +0300

    edit backend config

--- default.tf
+++ default.tf
@@ -4,11 +4,20 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = ">= 0.13"
+
   backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
     bucket = "tf-s3bucket-1"
+    region = "ru-central1"
     key    = "terraform.tfstate"
-    region = "ru-central1-a"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
   }
 }
 
