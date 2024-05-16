c015703f3ffa175f19771aa01dc8898e94f4d8bb
Vladislav <vlad.kanatik@ya.ru>
2024-05-16 12:05:05 +0300

    add basic configuration of yc with storage bucket

--- default.tf
+++ default.tf
@@ -2,11 +2,19 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
-      version = "0.118.0"
     }
   }
+  required_version = ">= 0.13"
+}
+
+locals {
+  cloud_id = "b1g1f73gcm5vet9spf42"
+  folder_id = "b1gds5v86pf14bpd23vd"
 }
 
 provider "yandex" {
-  # Configuration options
+  zone = "ru-central1-a"
+  cloud_id = local.cloud_id
+  folder_id = local.folder_id
+  service_account_key_file = "authorized_key.json"
 }
