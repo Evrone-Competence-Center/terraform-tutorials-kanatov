e0b114db6048cc864d153144e3649a978dfc7f2f
Vladislav <vlad.kanatik@ya.ru>
2024-05-17 10:36:41 +0300

    good decision

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 // Create SA
-resource "yandex_iam_service_account" "sa" {
+resource "yandex_iam_service_account" "sa-2" {
   folder_id = var.folder_id
   name      = "tf-test-sa"
 }
@@ -8,18 +8,18 @@ resource "yandex_iam_service_account" "sa" {
 resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
   folder_id = var.folder_id
   role      = "storage.editor"
-  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+  member    = "serviceAccount:${yandex_iam_service_account.sa-2.id}"
 }
 
 // Create Static Access Keys
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-  service_account_id = yandex_iam_service_account.sa.id
+resource "yandex_iam_service_account_static_access_key" "sa-2-static-key" {
+  service_account_id = yandex_iam_service_account.sa-2.id
   description        = "static access key for object storage"
 }
 
 // Use keys to create bucket
 resource "yandex_storage_bucket" "s3bucket-1" {
-  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket     = "tf-s3bucket-1"
+  access_key = yandex_iam_service_account_static_access_key.sa-2-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-2-static-key.secret_key
+  bucket     = "tf-s3bucket-2"
 }
