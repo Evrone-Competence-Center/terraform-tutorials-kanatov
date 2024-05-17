c4fe8ec2b14a8c767667e692a1922c4f0806c54a
Vladislav <vlad.kanatik@ya.ru>
2024-05-17 11:11:20 +0300

    rm vm creating

--- main.tf
+++ main.tf
@@ -24,37 +24,37 @@ resource "yandex_storage_bucket" "s3bucket-1" {
   bucket     = "tf-s3bucket-2"
 }
 
-resource "yandex_compute_instance" "vm-1" {
-  name        = "test"
-  platform_id = "standard-v1"
-  zone        = "ru-central1-a"
+# resource "yandex_compute_instance" "vm-1" {
+#   name        = "test"
+#   platform_id = "standard-v1"
+#   zone        = "ru-central1-a"
 
-  resources {
-    cores  = var.vms_cores
-    memory = var.vms_mem
-  }
+#   resources {
+#     cores  = var.vms_cores
+#     memory = var.vms_mem
+#   }
 
-  boot_disk {
-    initialize_params {
-      image_id = "fd8ejsdle3sqfpsgmqeh"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image_id = "fd8ejsdle3sqfpsgmqeh"
+#     }
+#   }
 
-  network_interface {
-    index     = 1
-    subnet_id = yandex_vpc_subnet.foo.id
-  }
+#   network_interface {
+#     index     = 1
+#     subnet_id = yandex_vpc_subnet.foo.id
+#   }
 
-  metadata = {
-    foo      = "bar"
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-  }
-}
+#   metadata = {
+#     foo      = "bar"
+#     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+#   }
+# }
 
-resource "yandex_vpc_network" "foo" {}
+# resource "yandex_vpc_network" "foo" {}
 
-resource "yandex_vpc_subnet" "foo" {
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.foo.id
-  v4_cidr_blocks = ["10.5.0.0/24"]
-}
+# resource "yandex_vpc_subnet" "foo" {
+#   zone           = "ru-central1-a"
+#   network_id     = yandex_vpc_network.foo.id
+#   v4_cidr_blocks = ["10.5.0.0/24"]
+# }
